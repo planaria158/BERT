c74f729c27d07a66e0602d6072b1255810208a9a
planaria158 <mark@arguslab.com>
2024-04-21 02:09:54 +0000

    refactoring the training code

---
+++ bert_lightning.py
@@ -0,0 +1,97 @@
+import torch
+from torch import nn
+from pytorch_lightning.core import LightningModule
+from bert_model import BERT
+
+#----------------------------------------------------------
+# Pytorch Lightning Module that hosts the BERT model
+# and runs the training, validation, and testing loops
+#----------------------------------------------------------
+class BERT_Lightning(LightningModule):
+    def __init__(self, config):
+        super(BERT_Lightning, self).__init__()
+        self.config = config
+        self.model = BERT(config)
+        self.criterion = nn.MSELoss()
+        self.save_hyperparameters()
+
+    def forward(self, x, mask):
+        return self.model(x, mask)
+
+    def common_forward(self, batch, batch_idx):
+        x, mask = batch
+        logits, loss = self.model(x, mask)
+        torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.config['grad_norm_clip'])
+        return logits, loss
+        # y_hat = torch.squeeze(y_hat)
+        # loss = self.criterion(y_hat, y)
+        # return loss, y_hat, y, transformer_out, attns, trans_input
+
+    def training_step(self, batch, batch_idx):
+        logits, loss = self.common_forward(batch, batch_idx)
+        self.log_dict({"loss": loss}, on_epoch=True, on_step=True, prog_bar=True, sync_dist=True)
+        return loss
+
+
+    # def on_validation_start(self):
+    #     self.y_preds = []
+    #     self.y_true = []
+    #     self.metrics = None
+
+    # def validation_step(self, batch, batch_idx):
+    #     val_loss, y_hat, y, _, _, _ = self.common_forward(batch, batch_idx)
+    #     self.y_true.extend(y.cpu().numpy().tolist())
+    #     self.y_preds.extend(y_hat.cpu().numpy().tolist())
+    #     self.log_dict({"val_loss": val_loss}, on_epoch=True, on_step=True, prog_bar=True, sync_dist=True)
+    #     self.logger.experiment.add_scalars('loss', {'valid': val_loss}, self.global_step)
+    #     return val_loss
+    
+    # def on_validation_end(self):
+    #     assert(len(self.y_preds) == len(self.y_true))
+    #     self.metrics = self._get_avm_metrics(self.y_true, self.y_preds)
+    #     mape_metrics = {'MAPE':self.metrics['MAPE'], 'mdAPE':self.metrics['mdAPE']}
+    #     ppe_metrics = {'PPE10':self.metrics['PPE10'], 'PPE20':self.metrics['PPE20']}
+    #     self.logger.experiment.add_scalars('mape', mape_metrics, self.current_epoch)
+    #     self.logger.experiment.add_scalars('ppe', ppe_metrics, self.current_epoch)
+    #     self.y_preds = None
+    #     self.y_true = None
+    #     return 
+
+    # def on_test_start(self):
+    #     self.y_preds = []
+    #     self.y_true = []
+    #     self.metrics = None
+
+    # def test_step(self, batch, batch_idx):
+    #     test_loss, y_hat, y, transformer_out, attns, trans_input = self.common_forward(batch, batch_idx)
+    #     self.log_dict({"test_loss": test_loss}, on_epoch=True, on_step=True, prog_bar=True, sync_dist=True)
+    #     self.logger.experiment.add_scalars('loss', {'test': test_loss},self.global_step)
+    #     self.y_true.extend(y.cpu().numpy().tolist())
+    #     self.y_preds.extend(y_hat.cpu().numpy().tolist())
+    #     return test_loss
+
+    # def on_test_end(self):
+    #     assert(len(self.y_preds) == len(self.y_true))
+    #     self.metrics = self._get_avm_metrics(self.y_true, self.y_preds)
+    #     print(self.metrics)
+    #     return 
+    
+    # Function to get evaluation metrics (MAPE, MdAPE, PPE10, PPE20)
+    # def _get_avm_metrics(self, y_true, y_pred):
+    #     y_true = np.exp(np.array(y_true))  # convert back to house prices.
+    #     y_pred = np.exp(np.array(y_pred))
+    #     ppe10 = np.count_nonzero((np.abs((np.divide(y_pred, y_true)) - 1) <= 0.1)) / y_pred.size
+    #     ppe20 = np.count_nonzero((np.abs((np.divide(y_pred, y_true)) - 1) <= 0.2)) / y_pred.size
+    #     mape  = (np.sum(np.abs(y_true - y_pred) / y_true))/len(y_true)
+    #     mdape = np.median(np.abs(y_true - y_pred)/y_true)
+
+    #     return {'MAPE': mape, 'mdAPE': mdape,
+    #             'PPE10': ppe10, 'PPE20': ppe20,
+    #             'Sample_Size': y_true.shape[0]}
+
+
+    def configure_optimizers(self):
+        lr = self.config['learning_rate']
+        optimizer = torch.optim.AdamW(self.model.parameters(), betas=self.config['betas'], lr=lr)
+        scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=self.config['lr_gamma'])
+        return [optimizer], [scheduler]
---
+++ fab_dataset.py
@@ -0,0 +1,69 @@
+import torch
+from torch.utils.data import Dataset
+import pandas as pd
+
+
+class FABSequenceDataset(Dataset):
+    """
+    Emits batches of characters
+    """
+    def __init__(self, config, csv_file_path, skiprows):  #pk_file_path):
+        self.config = config
+        print('reading the data from:', csv_file_path)
+        self.df = pd.read_csv(csv_file_path, skiprows=skiprows)
+        # self.df = pk.load(open(pk_file_path, 'rb'))
+        
+        # my_set = set()   
+        # def make_set(x):
+        #     for c in x:
+        #         my_set.add(c)
+
+        # self.df['Sequence'].apply(make_set)
+        # self.chars = sorted(list(my_set)) + ["[MASK]"]
+        # print('len of chars:', len(self.chars))
+        # print('chars:', self.chars)
+    
+        # 20 naturally occuring amino acids in human proteins plus MASK token
+        self.chars = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '[MASK]']
+        print('vocabulary:', self.chars)
+
+        data_size, vocab_size = self.df.shape[0], len(self.chars)
+        print('data has %d rows, %d vocab size (unique).' % (data_size, vocab_size))
+
+        self.stoi = { ch:i for i,ch in enumerate(self.chars) }
+        self.itos = { i:ch for i,ch in enumerate(self.chars) }
+        self.vocab_size = vocab_size
+
+    def get_vocab_size(self):
+        return self.vocab_size
+
+    def get_block_size(self):
+        return self.config['block_size']
+
+    def __len__(self):
+        return self.df.shape[0] #len(self.data) - self.config['block_size']
+
+    """ Returns data, mask pairs used for Masked Language Model training """
+    def __getitem__(self, idx):
+        # grab a chunk of (block_size) characters from the data
+        # chunk = self.data[idx:idx + self.config['block_size']]
+        chunk = self.df.loc[idx, 'Sequence']
+        
+        # encode every character to an integer
+        dix = torch.tensor([self.stoi[s] for s in chunk], dtype=torch.long)
+
+        # get number of tokens to mask
+        n_pred = max(1, int(round(self.config['block_size']*self.config['mask_prob'])))
+
+        # indices of the tokens that will be masked (a random selection of n_pred of the tokens)
+        masked_idx = torch.randperm(self.config['block_size'], dtype=torch.long, )[:n_pred]
+
+        mask = torch.zeros_like(dix)
+
+        # copy the actual tokens to the mask
+        mask[masked_idx] = dix[masked_idx]
+        
+        # ... and overwrite then with MASK token in the data
+        dix[masked_idx] = self.stoi["[MASK]"]
+
+        return dix, mask 
